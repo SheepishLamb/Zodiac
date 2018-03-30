bcd757ab2d40611f1612e32976396fc477822d8a
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-03-30 00:02:53 -0600

    Add files via upload

--- characters.php
+++ characters.php
@@ -49,7 +49,7 @@
 	
 	<!-- Character 1 -->
      <div id="character1a" class="tab" style="display: none;">	
-<h1>Willard Yijun</h1>	 
+<div style="text-align: center; font-family: fredericka the great, cursive; font-size: 42pt; font-weight: normal;">Willard Yijun</div>
 				<table style="font-size: 12pt; margin: auto; text-align: center;">
 				<tr>
 				<td style="background: #00000033;">Race</td><td style="background: #4d054e33;">Human</td>
@@ -87,7 +87,7 @@
 				  
 				  <!-- Character 2 -->				  
 				  <div id="character2a" class="tab" style="display: none;">
-<h1>Qīngtíng (Dragonfly)</h1>				  
+<div style="text-align: center; font-family: fredericka the great, cursive; font-size: 42pt; font-weight: normal;">Qīngtíng (Dragonfly)</div>				  
 				<table style="font-size: 12pt; margin: auto; text-align: center;">
 				<tr>
 				<td style="background: #00000033;">Race</td><td style="background: #4d054e33;">Human</td>
@@ -124,7 +124,7 @@
 				  
 				  <!-- Character 3 -->				  
 				  <div id="character3a" class="tab" style="display: none;">	
-<h1>Gustavo "Gus" Gibson or G.G.</h1>				  
+<div style="text-align: center; font-family: fredericka the great, cursive; font-size: 42pt; font-weight: normal;">Gustavo "Gus" Gibson or G.G.</div>				  
 				<table style="font-size: 12pt; margin: auto; text-align: center;">
 				<tr>
 				<td style="background: #00000033;">Race</td><td style="background: #4d054e33;">Human</td>
@@ -161,7 +161,7 @@
 				  
 				  <!-- Character 4 -->				  
 				  <div id="character4a" class="tab" style="display: none;">		
-<h1>Bellamy</h1>				  
+<div style="text-align: center; font-family: fredericka the great, cursive; font-size: 42pt; font-weight: normal;">Bellamy</div>				  
 				<table style="font-size: 12pt; margin: auto; text-align: center;">
 				<tr>
 				<td style="background: #00000033;">Race</td><td style="background: #4d054e33;">Human</td>
@@ -198,7 +198,7 @@
 				  
 				  <!-- Character 5 -->				  
 				  <div id="character5a" class="tab" style="display: none;">		
-<h1>Steve</h1>				  
+<div style="text-align: center; font-family: fredericka the great, cursive; font-size: 42pt; font-weight: normal;">Steve</div>				  
 				<table style="font-size: 12pt; margin: auto; text-align: center;">
 				<tr>
 				<td style="background: #00000033;">Race</td><td style="background: #4d054e33;">Human</td>
@@ -267,139 +267,120 @@
 			<div class="tabs">
     <ul class="tab-links">
       <li id="npc1"><a href="#npc1a">Diego Kù</a></li>
+	  <li id="npc2"><a href="#npc2a">Grandfather Clock</a></li>
+	  <li id="npc3"><a href="#npc3a">Grandmother Clock</a></li>
     </ul>
 	
 	<div class="tab-content">
 	
 	<!-- NPC 1 -->
-     <div id="npc1a" class="tab" style="display: none;">				
-				<table style="font-size: 12pt; width: 625px; margin: auto;">
+     <div id="npc1a" class="tab" style="display: none;">
+<div style="text-align: center; font-family: fredericka the great, cursive; font-size: 42pt; font-weight: normal;">Diego Kù</div>
+<table style="font-size: 12pt; margin: auto; text-align: center;">
 				<tr>
-				<td>Race: Human
-				</td>
-				<td>Class/Level: ??/15
-				</td>
-				<td>Alignment: Lawful Good
-				</td>
-				<td>Residence: Xicheng Hub
-				</td>
+				<td style="background: #00000033;">Race</td><td style="background: #4d054e33;">Human</td>
+				<td style="background: #00000033;">Class - Level</td><td style="background: #4d054e33;">?? - 15</td>
+				<td style="background: #00000033;">Alignment</td><td style="background: #4d054e33;">Lawful Good</td>
+				<td style="background: #00000033;">Alligence</td><td style="background: #4d054e33;">Xicheng Hub</td>
 				</tr>
 				<tr>
-				<td>Occupation: Hub Leader
-				</td>
-				<td>Date of Birth: 2034
-				</td>
-				<td>Nationality: Chinese
-				</td>
-				<td>Gender: Male
-				</td>
+				<td style="background: #00000033;">Residence</td><td style="background: #4d054e33;">Beijing</td>
+				<td style="background: #00000033;">Current Occupation</td><td style="background: #4d054e33;">Hub Leader</td>
+				<td style="background: #00000033;">Date of Birth</td><td style="background: #4d054e33;">2034</td>
+				<td style="background: #00000033;">Place of Birth</td><td style="background: #4d054e33;">Beijing, China</td>
 				</tr>
 				<tr>
-				<td>Age: early 40s
-				</td>
-				<td>Body: Slim/Athletic Build
-				</td>
-				<td>Hair: Black
-				</td>
-				<td>Eyes: Black
-				</td>
+				<td style="background: #00000033;">Nationality</td><td style="background: #4d054e33;">Chinese</td>
+				<td style="background: #00000033;">Gender</td><td style="background: #4d054e33;">Male</td>
+				<td style="background: #00000033;">Age</td><td style="background: #4d054e33;">Early 40s</td>
+				<td style="background: #00000033;">Body</td><td style="background: #4d054e33;">Slim/Athletic Build</td>
 				</tr>
+				<tr>
+				<td style="background: #00000033;">Height</td><td style="background: #4d054e33;">5'6"</td>
+				<td style="background: #00000033;">Hair</td><td style="background: #4d054e33;">Black</td>
+				<td style="background: #00000033;">Eyes</td><td style="background: #4d054e33;">Black</td>
 				</table>
 				
 				<b style="font-size: 18pt;">Summary</b>
                   <ul style="font-size: 10pt;">
 				  <li class="chparagraph">
 				  <img id="chimg51" class="img" src="companylogo.png">
-				  <b>A man known as “The Silent”. (This is mostly because when a problem arises in the district, he prefers action over conversation.)</b></li>
+				  <b>A man known as “The Silent”. (This is mostly because when a problem arises in the district, he prefers action over conversation.)
+				  <br/>After an early life of crime, living on the streets of Beijing, Deigo Kù is the current leader of the Xicheng Hub. He oversees the mission assignments and rank promotions within the Hub. All ranks Year One through Five report to him. He lives in the top of the Palace of Heavenly Purity, and can usually be found in The Hub during operational hours.</b></li>
                   </ul>
 				  </div>
 				  <!-- NPC 1 - End -->
 				  
-				  
-				  <!-- NPC 2 -->				  
-				  <div id="npc2a" class="tab" style="display: none;">				
-				<table style="font-size: 12pt; width: 625px; margin: auto;">
+				  <!-- NPC 2 -->
+     <div id="npc2a" class="tab" style="display: none;">
+<div style="text-align: center; font-family: fredericka the great, cursive; font-size: 42pt; font-weight: normal;">Grandfather Clock</div>
+<table style="font-size: 12pt; margin: auto; text-align: center;">
 				<tr>
-				<td>Race 2
-				</td>
-				<td>Class/Level
-				</td>
-				<td>Alignment
-				</td>
-				<td>Residence
-				</td>
+				<td style="background: #00000033;">Race</td><td style="background: #4d054e33;">Human</td>
+				<td style="background: #00000033;">Class - Level</td><td style="background: #4d054e33;">?? - 9</td>
+				<td style="background: #00000033;">Alignment</td><td style="background: #4d054e33;">Neutral Good</td>
+				<td style="background: #00000033;">Alligence</td><td style="background: #4d054e33;">Xicheng Hub</td>
 				</tr>
 				<tr>
-				<td>Occupation
-				</td>
-				<td>Date of Birth
-				</td>
-				<td>Nationality
-				</td>
-				<td>Gender
-				</td>
+				<td style="background: #00000033;">Residence</td><td style="background: #4d054e33;">Beijing</td>
+				<td style="background: #00000033;">Current Occupation</td><td style="background: #4d054e33;">Hub Supplier</td>
+				<td style="background: #00000033;">Date of Birth</td><td style="background: #4d054e33;">2018</td>
+				<td style="background: #00000033;">Place of Birth</td><td style="background: #4d054e33;">New York City, NY, USA</td>
 				</tr>
 				<tr>
-				<td>Age
-				</td>
-				<td>Weight/Height
-				</td>
-				<td>Hair
-				</td>
-				<td>Eyes
-				</td>
+				<td style="background: #00000033;">Nationality</td><td style="background: #4d054e33;">American</td>
+				<td style="background: #00000033;">Gender</td><td style="background: #4d054e33;">Male</td>
+				<td style="background: #00000033;">Age</td><td style="background: #4d054e33;">70s</td>
+				<td style="background: #00000033;">Body</td><td style="background: #4d054e33;">Elder/Average Build</td>
 				</tr>
+				<tr>
+				<td style="background: #00000033;">Height</td><td style="background: #4d054e33;">5'5"</td>
+				<td style="background: #00000033;">Hair</td><td style="background: #4d054e33;">White</td>
+				<td style="background: #00000033;">Eyes</td><td style="background: #4d054e33;">Bluish-Grey</td>
 				</table>
 				
 				<b style="font-size: 18pt;">Summary</b>
                   <ul style="font-size: 10pt;">
 				  <li class="chparagraph">
 				  <img id="chimg6" class="img" src="companylogo.png">
-				  <b>A man known as “The Silent”. (This is mostly because when a problem arises in the district, he prefers action over conversation.)</b></li>
+				  <b>The Hub's Supplier. Grandfather Clock can always be found in Fengxian Hall. He is affectionately named "Grandfather Clock" for the near-eccentric amount of cuckoo clocks and watches that adorn the walls and shelves of his shop. The Clocks (Grandfather and Grandmother Clock) both live in the back of the Hall, and have dedicated their efforts toward supplying Xicheng Hub with whatever bits and bobs they require, often trading with the Beijing Buddhists in exchange for favors from the Hub members. Grandfather Clock is the oldest member of the Xicheng Hub, but dispite his age, he seems to know his way around the electronic items the members equipt.</b></li>
                   </ul>
 				  </div>
 				  <!-- NPC 2 - End -->
 				  
-				  <!-- NPC 3 -->				  
-				  <div id="npc3a" class="tab" style="display: none;">				
-				<table style="font-size: 12pt; width: 625px; margin: auto;">
+				  <!-- NPC 3 -->
+     <div id="npc3a" class="tab" style="display: none;">
+<div style="text-align: center; font-family: fredericka the great, cursive; font-size: 42pt; font-weight: normal;">Grandmother Clock</div>
+<table style="font-size: 12pt; margin: auto; text-align: center;">
 				<tr>
-				<td>Race 3
-				</td>
-				<td>Class/Level
-				</td>
-				<td>Alignment
-				</td>
-				<td>Residence
-				</td>
+				<td style="background: #00000033;">Race</td><td style="background: #4d054e33;">Human</td>
+				<td style="background: #00000033;">Class - Level</td><td style="background: #4d054e33;">?? - 5</td>
+				<td style="background: #00000033;">Alignment</td><td style="background: #4d054e33;">Lawful Good</td>
+				<td style="background: #00000033;">Alligence</td><td style="background: #4d054e33;">Xicheng Hub</td>
 				</tr>
 				<tr>
-				<td>Occupation
-				</td>
-				<td>Date of Birth
-				</td>
-				<td>Nationality
-				</td>
-				<td>Gender
-				</td>
+				<td style="background: #00000033;">Residence</td><td style="background: #4d054e33;">Beijing</td>
+				<td style="background: #00000033;">Current Occupation</td><td style="background: #4d054e33;">Hub Supplier</td>
+				<td style="background: #00000033;">Date of Birth</td><td style="background: #4d054e33;">2022</td>
+				<td style="background: #00000033;">Place of Birth</td><td style="background: #4d054e33;">Manhatten, NY, USA</td>
 				</tr>
 				<tr>
-				<td>Age
-				</td>
-				<td>Weight/Height
-				</td>
-				<td>Hair
-				</td>
-				<td>Eyes
-				</td>
+				<td style="background: #00000033;">Nationality</td><td style="background: #4d054e33;">American</td>
+				<td style="background: #00000033;">Gender</td><td style="background: #4d054e33;">Female</td>
+				<td style="background: #00000033;">Age</td><td style="background: #4d054e33;">70s</td>
+				<td style="background: #00000033;">Body</td><td style="background: #4d054e33;">Elder/Average Build</td>
 				</tr>
+				<tr>
+				<td style="background: #00000033;">Height</td><td style="background: #4d054e33;">5'2"</td>
+				<td style="background: #00000033;">Hair</td><td style="background: #4d054e33;">White</td>
+				<td style="background: #00000033;">Eyes</td><td style="background: #4d054e33;">Green</td>
 				</table>
 				
-				<b style="font-size: 18pt;">Backstory</b>
+				<b style="font-size: 18pt;">Summary</b>
                   <ul style="font-size: 10pt;">
 				  <li class="chparagraph">
 				  <img id="chimg7" class="img" src="companylogo.png">
-				  <b>It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.</b></li>
+				  <b>The Hub's Supplier. Married to Grandfather Clock. Not much is known about her, except that she remembers every guild member's birthday and always offers tea and cookies to any visitors.</b></li>
                   </ul>
 				  </div>
 				  <!-- NPC 3 - End -->

0d8e1a219dcc382bda9cdab2c35249f68ef80b5c
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-04-06 21:59:07 -0600

    Add files via upload

--- shop.php
+++ shop.php
@@ -9,32 +9,21 @@
 	<br/>
 	<br/>
 	
-	
-	Armor
+	<span style="font-size: 14pt;">Miscellaneous</span>
 	<table style="margin: auto;">
 	<tr class="title"><td class="item">Item</td><td class="cost">Cost</td><td class="desc">Description</td></tr>
-	<tr><td class="item">1</td><td class="cost">&#9566;500</td><td class="desc">Yaddayadda</td></tr>
-	<tr><td class="item">2</td><td class="cost">&#9566;500</td><td class="desc">Yaddayadda</td></tr>
-	<tr><td class="item">3</td><td class="cost">&#9566;500</td><td class="desc">Yaddayadda</td></tr>
+	<tr><td class="item">Watch Batteries</td><td class="cost">&#9566;100</td><td class="desc">Non-Rechargable Mechanical, Digital, and Smartwatch batteries that last a total of 12 days.</td></tr>
 	</table>
 	<br/>
-	Weaponry
-	<table style="margin: auto;">
-	<tr class="title"><td class="item">Item</td><td class="cost">Cost</td><td class="desc">Description</td></tr>
-	<tr><td class="item">1</td><td class="cost">&#9566;500</td><td class="desc">Yaddayadda</td></tr>
-	<tr><td class="item">2</td><td class="cost">&#9566;500</td><td class="desc">Yaddayadda</td></tr>
-	<tr><td class="item">3</td><td class="cost">&#9566;500</td><td class="desc">Yaddayadda</td></tr>
-	</table>
-	<br/>
-	Electronics
+	<span style="font-size: 14pt;">Electronics</span>
 	<table style="margin: auto;">
 	<tr class="title"><td class="item">Item</td><td class="cost">Cost</td><td class="desc">Description</td></tr>
 	<tr><td class="item">"Zune" MP3 Player</td><td class="cost">&#9566;200</td><td class="desc">An archaic and <i>failed</i> brand of digital/portable music players from the early 2000s.</td></tr>
-	<tr><td class="item">2</td><td class="cost">&#9566;500</td><td class="desc">Yaddayadda</td></tr>
-	<tr><td class="item">3</td><td class="cost">&#9566;500</td><td class="desc">Yaddayadda</td></tr>
+	<tr><td class="item">Smartwatch</td><td class="cost">&#9566;50000</td><td class="desc">A wifi-capable wristwatch that can connect you to the rest of the world through social media. Downloadable apps available for purchase.</td></tr>
+	<tr><td class="item">Smartglasses</td><td class="cost">&#9566;50000</td><td class="desc">Wifi-capable glasses with a built-in mini-projector. Watch the weather forecast through your periphery while reading restaurant reviews that hang over the building in real-time! Downloadable apps available for purchase.</td></tr>
 	</table>
 	<br/>
-	Glasses Languages
+	<span style="font-size: 14pt;">Glasses Languages</span>
 	<table style="margin: auto;">
 	<tr class="title"><td class="item">Item</td><td class="cost">Cost</td><td class="desc">Description</td></tr>
 	<tr><td class="item">Spanish</td><td class="cost">&#9566;250</td><td class="desc">su casa es mi queso</td></tr>
@@ -51,7 +40,7 @@
 	</table>
 	<br/>
 	<br/>
-	Apps
+	<span style="font-size: 14pt;">Apps</span>
 	<br/>W = Watch | G = Glasses
 	<table style="margin: auto;">
 	<tr class="title"><td class="item">Item</td><td class="apptitle appcheck">W</td><td class="apptitle appcheck">G</td><td class="cost">Cost</td><td class="desc">Description</td></tr>
@@ -60,10 +49,10 @@
 	<tr><td class="item">Fast Food Delivery</td><td class="appcheck">✓</td><td class="appcheck">&nbsp;</td><td class="cost">&#9566;1000</td><td class="desc">A food ordering app that can resupply rations once per week via drone delivery service.</td></tr>
 	<tr><td class="item">App of Disguise</td><td class="appcheck">✓</td><td class="appcheck">✓</td><td class="cost">&#9566;1000</td><td class="desc">A full-body image projector. Can disguise self with a selection of pre-loaded images.<br/><span style="font-size: 10pt;">(Male Garbage Truck Worker, Female Social Media Journalist, Female Nurse w/ blue scrubs, Male Construction Site Worker, Grandfather Clock, Grandmother Clock, Female CEO, Male CEO)</span></td></tr>
 	<tr><td class="item">Sonar</td><td class="appcheck">✓</td><td class="appcheck">✓</td><td class="cost">&#9566;1000</td><td class="desc">A grid-based overlay that allows you to spot enemies around corners and through walls in radius of 20ft.</td></tr>
-	<tr><td class="item">Sonar+</td><td class="appcheck">✓</td><td class="appcheck">✓</td><td class="cost">&#9566;1000</td><td class="desc">Requires: Sonar. Extends radius to 40ft.</td></tr>
-	<tr><td class="item">Sonar++</td><td class="appcheck">✓</td><td class="appcheck">✓</td><td class="cost">&#9566;1000</td><td class="desc">Requires: Sonar and Sonar+. Extends radius to 60ft.</td></tr>
-	<tr><td class="item">Learn by Sleep</td><td class="appcheck">✓</td><td class="appcheck">&nbsp;</td><td class="cost">&#9566;1000</td><td class="desc">If left on overnight, gives +2 to chosen skill during the next day.</td></tr>
-	<tr><td class="item">Did It Hurt?</td><td class="appcheck">✓</td><td class="appcheck">&nbsp;</td><td class="cost">&#9566;1000</td><td class="desc">When speaking to character of the preferred gender, 25% chance of app prompting a pickup line, giving +3 to diplomacy. 5% chance of pickup line bombing.</td></tr>
+	<tr><td class="item">Sonar+</td><td class="appcheck">✓</td><td class="appcheck">✓</td><td class="cost">&#9566;1000</td><td class="desc">Requires: Sonar.<br/>Extends radius to 40ft.</td></tr>
+	<tr><td class="item">Sonar++</td><td class="appcheck">✓</td><td class="appcheck">✓</td><td class="cost">&#9566;1000</td><td class="desc">Requires: Sonar and Sonar+.<br/>Extends radius to 60ft.</td></tr>
+	<tr><td class="item">Learn by Sleep</td><td class="appcheck">✓</td><td class="appcheck">&nbsp;</td><td class="cost">&#9566;1000</td><td class="desc">If left on overnight, player can train a skill during sleep. Gives +2 to chosen skill during the next day.</td></tr>
+	<tr><td class="item">Did It Hurt?</td><td class="appcheck">&nbsp;</td><td class="appcheck">✓</td><td class="cost">&#9566;1000</td><td class="desc">When speaking to character of the preferred gender, 25% chance of app prompting a pickup line, giving +3 to diplomacy. 5% chance of pickup line bombing.</td></tr>
 	
 	</table>
 	<br/>

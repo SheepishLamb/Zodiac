1c19d09230c05236e30d63f19f099fa1375d93c7
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-04-06 20:25:04 -0600

    Add files via upload

--- shop.php
+++ shop.php
@@ -1,7 +1,10 @@
 <div id="shcontainer">
-<h1 style="font-family: 'Libre Barcode 128 Text', cursive; font-size: 92px; text-align: center; line-height: 12pt; font-weight: 400;">Shop</h1>			
+<h1 style="font-family: 'Libre Barcode 128 Text', cursive; font-size: 92px; text-align: center; line-height: 12pt; font-weight: 400;">Shop</h1>
+			
 			
     <div class="shparagraph">
+	Grandfather Clock offers the full assortment of Equipment <span style="font-size:10pt;">(See: D20 Modern Core Rulebook, pg. 96-127)</span> as well as a few unique items below. <br/><br/>
+	Armor
 	<table style="margin: auto;">
 	<tr><td>Item</td><td>Cost</td><td>Description</td></tr>
 	<tr><td>1</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
@@ -10,6 +13,53 @@
 	<tr><td>4</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
 	<tr><td>5</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
 	</table>
+	<br/>
+	Weaponry
+	<table style="margin: auto;">
+	<tr><td>Item</td><td>Cost</td><td>Description</td></tr>
+	<tr><td>1</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>2</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>3</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>4</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>5</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	</table>
+	<br/>
+	Electronics
+	<table style="margin: auto;">
+	<tr><td>Item</td><td>Cost</td><td>Description</td></tr>
+	<tr><td>"Zune" MP3 Player</td><td>&#9566;200</td><td>An archaic and <i>failed</i> brand of digital/portable music players from the early 2000s.</td></tr>
+	<tr><td>2</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>3</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>4</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>5</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	</table>
+	<br/>
+	Watch Apps
+	<table style="margin: auto;">
+	<tr><td>Item</td><td>Cost</td><td>Description</td></tr>
+	<tr><td>1</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>2</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>3</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>4</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	<tr><td>5</td><td>&#9566;500</td><td>Yaddayadda</td></tr>
+	</table>
+	<br/>
+	Glasses Languages
+	<table style="margin: auto;">
+	<tr><td>Item</td><td>Cost</td><td>Description</td></tr>
+	<tr><td>Spanish</td><td>&#9566;250</td><td>su casa es mi queso</td></tr>
+	<tr><td>French</td><td>&#9566;250</td><td>omelette du fromage</td></tr>
+	<tr><td>Hindi</td><td>&#9566;250</td><td>गाय रंभाती है</td></tr>
+	<tr><td>Arabic</td><td>&#9566;250</td><td>أنا أبكي كثيرا</td></tr>
+	<tr><td>Portuguese</td><td>&#9566;250</td><td>portar um gansos</td></tr>
+	<tr><td>Bengali</td><td>&#9566;250</td><td>আমার বিছানা মধ্যে একটি দৈত্য আছে</td></tr>
+	<tr><td>Russian</td><td>&#9566;250</td><td>медведи на велосипедах с одним колесом</td></tr>
+	<tr><td>Japanese</td><td>&#9566;250</td><td>触手モンスター</td></tr>
+	<tr><td>German</td><td>&#9566;250</td><td>wir bären Bären</td></tr>
+	<tr><td>Greek</td><td>&#9566;250</td><td>Ο Δίας θέλει να ντυθεί σαν ζώο</td></tr>
+	<tr><td>Celtic</td><td>&#9566;250</td><td>Is slag é do mháthair</td></tr>
+	</table>
+	<br/>
 </div>	
 				  
 

6a46c6be8b9ce6bab1db583487185cd4c19e144d
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-04-19 13:22:43 -0600

    Add files via upload

--- maps.php
+++ maps.php
@@ -8,21 +8,36 @@
 
 <div hidden id="mpimg2a" class="mpimga">
 <div class="mpimgclose">X</div>
-<img id="mpimg2b" class="mpimgb" src="smartwatch.png">
+<img id="mpimg2b" class="mpimgb" src="baitasi.png">
+</div>
+
+<div hidden id="mpimg3a" class="mpimga">
+<div class="mpimgclose">X</div>
+<img id="mpimg3b" class="mpimgb" src="lotusblue.jpg">
+</div>
+
+<div hidden id="mpimg4a" class="mpimga">
+<div class="mpimgclose">X</div>
+<img id="mpimg4b" class="mpimgb" src="bmonh.jpg">
+</div>
+
+<div hidden id="mpimg5a" class="mpimga">
+<div class="mpimgclose">X</div>
+<img id="mpimg5b" class="mpimgb" src="warehouse.jpg">
 </div>
 
 
 <h1 style="font-family: 'Libre Barcode 128 Text', cursive; font-size: 92px; text-align: center; line-height: 12pt; font-weight: 400;">Settings</h1>			
 	
-<div><span style="color: red;">The year is 2078 - Year of the Earth Dog</span>
-<br/><br/>Rolatide Incorporated - the company responsible for the breakthrough in Alzheimer's treatment, along with a whole slew of medications, vitamins, and the jump in use of nanotechnology by the year 2050 -  has bought out most, if not all, of the competing companies in Beijing, China, causing a massive amount of job losses as the monopoly grew. Still needing credits to keep roofs over their heads and food on their plates, mercenary work quickly became the norm for the unemployed in Beijing. Whatever needed to be done with no questions asked, would be done - for a price. Quickly following were The Hubs - organized guilds for those Jack-of-all-Trades that would offer protection, equipment, and a steady stream of jobs for a small cut of the profits from each gig.
-<br/><br/>Your Hub is located in the Xicheng district of Beijing - namely <i>The Forbidden City</i>.
-<br/><br/>The Xicheng Hub is run by Diego Kù - a man known as “<span style="color: babyblue;">The Silent</span>”. (This is mostly because when a problem arises in the district, he prefers action over conversation.) Out of all the Hubs in Beijing, it seems that the Xicheng Hub has the best resources and a wider clientele for jobs. You’ve all been with the Hub for a year now, and have earned the rank “<span style="color: goldenrod;">Nián Yī</span>” (or “Year One”), as well as each of you granted a room in the Palace of Heavenly Purity - a tower located just behind the Hub’s Headquarters where long-standing members live. Some of the more senior members have whole floors to themselves throughout the tower, but the majority have loft-style apartments that take up ¼ of a single floor, sharing the floor with 3 other apartments.
+<div style="font-size: 14pt; font-family: vt323;"><span style="color: red;">The year is 2078 - Year of the Earth Dog</span>
+<br/><br/><span style="color: yellowgreen;">Rolatide Incorporated</span> - the company responsible for the breakthrough in Alzheimer's treatment, along with a whole slew of medications, vitamins, and the jump in use of nanotechnology by the year 2050 -  has bought out most, if not all, of the competing companies in Beijing, China, causing a massive amount of job losses as the monopoly grew. Still needing credits to keep roofs over their heads and food on their plates, mercenary work quickly became the norm for the unemployed in Beijing. Whatever needed to be done with no questions asked, would be done - for a price. Quickly following were The Hubs - organized guilds for those Jack-of-all-Trades that would offer protection, equipment, and a steady stream of jobs for a small cut of the profits from each gig.
+<br/><br/>Your Hub is located in the Xicheng district of Beijing - namely <i style="color: yellowgreen;">The Forbidden City</i>.
+<br/><br/>The Xicheng Hub is run by Diego Kù - a man known as “<span style="color: lightskyblue;">The Silent</span>”. (This is mostly because when a problem arises in the district, he prefers action over conversation.) Out of all the Hubs in Beijing, it seems that the Xicheng Hub has the best resources and a wider clientele for jobs. You’ve all been with the Hub for a year now, and have earned the rank “<span style="color: palegoldenrod;">Nián Yī</span>” (or “Year One”), as well as each of you granted a room in the Palace of Heavenly Purity - a tower located just behind the Hub’s Headquarters where long-standing members live. Some of the more senior members have whole floors to themselves throughout the tower, but the majority have loft-style apartments that take up ¼ of a single floor, sharing the floor with 3 other apartments.
 <br/><br/>In exchange for running with the Hub, you’ve all negotiated to pay 5% of whatever you rake in on your jobs until such a time you decide to part ways with the guild (this is automatically calculated by me). The benefits of membership include exclusive gear and upgrades to equipment for a discounted cost versus what you would find outside of the walls. You can find the guild suppliers in Fengxian Hall. 
 <br/><br/>The Forbidden City not only houses the Hub, but is of course a small community that allows a few dozen families and businesses within the walls. The people who live within the walls often employ the Hub’s members for errands or occasional protective duties for which they get paid and report back to the Hub. More common are missions that take place around Beijing for higher prices. Very rarely, if at all, will a Hub member be deployed outside of Beijing. This is so as to not cross into other Hub territories, unless two or more Hubs have agreed to work together.
 <br/><br/>The Forbidden City itself is named that way because hundreds and hundreds of years ago, this is where the Chinese emperors would live. The area was strictly forbidden to commoners. The emperor and his concubines as well as servants and their families would live inside of these walls. It was the political center of China for about 500 years before it became essentially a museum in the early 20th century. Tourists stopped coming around and the area’s been abandoned for decades until the Hubs were formed. The high walls made it the perfect place to host a guild with security and privacy in mind.
 </div>	
-	
+	<br/>
 	<div id="mpicons">
 	<table style="text-align: center; margin: auto;">
 	<tr>
@@ -35,7 +50,62 @@
 	</td>
 	</tr>
 	</table>
-	
+   </div>
+   
+   <div id="mpicons">
+	<table style="text-align: center; margin: auto;">
+	<tr>
+	<td>
+	<img id="mpimg2" class="mpimg" src="baitaisi.png">
+	</td>
+	</tr>
+	<tr>
+	<td>Baita Si
+	</td>
+	</tr>
+	</table>
+   </div>
+   
+   <div id="mpicons">
+	<table style="text-align: center; margin: auto;">
+	<tr>
+	<td>
+	<img id="mpimg3" class="mpimg" src="lotusblue.jpg">
+	</td>
+	</tr>
+	<tr>
+	<td>Lotus Blue
+	</td>
+	</tr>
+	</table>
+   </div>
+   
+   <div id="mpicons">
+	<table style="text-align: center; margin: auto;">
+	<tr>
+	<td>
+	<img id="mpimg4" class="mpimg" src="bmonh.jpg">
+	</td>
+	</tr>
+	<tr>
+	<td>Beijing Museum of Natural History
+	</td>
+	</tr>
+	</table>
+   </div>
+   
+   <div id="mpicons">
+	<table style="text-align: center; margin: auto;">
+	<tr>
+	<td>
+	<img id="mpimg5" class="mpimg" src="warehouse.jpg">
+	</td>
+	</tr>
+	<tr>
+	<td>Nike Warehouse
+	</td>
+	</tr>
+	</table>
    </div>
 				  
 				
@@ -51,6 +121,15 @@ $(function() {
 	});
 	$('#mpimg2').click(function(e) {
 		$('#mpimg2a').toggle();
+	});
+	$('#mpimg3').click(function(e) {
+		$('#mpimg3a').toggle();
+	});
+	$('#mpimg4').click(function(e) {
+		$('#mpimg4a').toggle();
+	});
+	$('#mpimg5').click(function(e) {
+		$('#mpimg5a').toggle();
 	});
 	  $('.mpimgclose').click(function(e) {
 		$(this).parent('.mpimga').css('display', 'none');
@@ -125,11 +204,12 @@ $(function() {
 }
 
 .mpimgclose {
-	position: absolute;
+	position: fixed;
     top: 10px;
-    right: 10px;
+    right: 100px;
     font-weight: bold;
     font-size: 14pt;
+    color: orange;
 }
 
 </style>

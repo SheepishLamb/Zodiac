bc60a64a2d546cb5fe31b24c05073addd601454e
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-04-23 20:34:53 -0600

    Add files via upload

--- inchworm.php
+++ inchworm.php
@@ -1,6 +1,6 @@
 <div class="artcontainer">
 <h1 style="font-family: 'VT323', monospace; font-size: 28px; text-align: center; line-height: 20pt; font-weight: 400;">Transcription 273
-<br/>Meeting Date 4-26-2078
+<br/>Meeting Date 4-26-2072
 <br/>R̪̘̫̯̼ͤͭ̒̃͆ͨ̀ Member Discussion
 <br/>Beijing, China
 <br/><br/>
@@ -14,15 +14,15 @@ S.L.: Alright. I’m sure most of you have noticed the reports of the two missin
 <br/><br/>
 C.F., J.͙̣̘̬̗̠̫̌ͦ̊͂L̬̦͇̹͙̜̩̐̕., F.G., L.M., B.G.: Yes.
 <br/><br/>
-S.L.: We’ve speculated that Rolatide is behind these robberies, and now thanks for some reconnaissance, we’re able to confirm that. They may have wiped the tape from the museum to cover their tracks, but luckily H͔̞͚̗͔ͨͪ͂̅é̖c̟̩͓̲͖̞̗̊̑̌͂ͤ̎̋t͎͎ͫͬͬọ͕̱̝͓̫̟̇͆ͪ̒̋̈͠r͕̠͈̱̭̲̲ͧ̓̄͌̓̄̈͡ was able to get us access to the list of MAC addresses that accessed the museum wireless signal during the week before the first news report aired. Celeste?
+S.L.: We’ve speculated that Rolatide is behind these robberies, and now thanks for some reconnaissance, we’re able to confirm that. They may have wiped the tape from the library to cover their tracks, but luckily H͔̞͚̗͔ͨͪ͂̅é̖c̟̩͓̲͖̞̗̊̑̌͂ͤ̎̋t͎͎ͫͬͬọ͕̱̝͓̫̟̇͆ͪ̒̋̈͠r͕̠͈̱̭̲̲ͧ̓̄͌̓̄̈͡ was able to get us access to the list of MAC addresses that accessed the library wireless signal during the week before the first news report aired. Celeste?
 <br/><br/>
-C.F.: There’s a couple repeats during the few days leading up to the robbery. I checked them out, and they match the devices we found listed at the factories and the d̢̝͈̳͉̠ͪ̆͑̇̔ͥỏ̉͐̔̓w̢͕̞͎̰̽͋͂ͯͤ̌̚ñ͇̫͓̰͗̂t͎͔̳̹̗̤̻͑͛̽̀͆͌o̰ͫ͗̃ͧͩ̌̓w̤͎̙̭͔̮ͫ͊̆͊͆̃̉n͎̣̫̞͜ͅ ̰̺͊̽̈́̍ͭ́͝l̢̎i̻̮̥̭ͥ̊ͧͭ̽͑ḅ͈͔̖̟͟r̜͙̝ͨͅa̵̼͙̒ͦͧ̊ͤ̊ͫͅr̢͈̞͕͉͓̍y̖̟̹̮͖͓̤̅͒ͭ͆̚. We have more than enough reason to trust that these are Rolatide devices.
+C.F.: There’s a couple repeats during the few days leading up to the robbery. I checked them out, and they match the devices we found listed at the factories and the d̢̝͈̳͉̠ͪ̆͑̇̔ͥỏ̉͐̔̓w̢͕̞͎̰̽͋͂ͯͤ̌̚ñ͇̫͓̰͗̂t͎͔̳̹̗̤̻͑͛̽̀͆͌o̰ͫ͗̃ͧͩ̌̓w̤͎̙̭͔̮ͫ͊̆͊͆̃̉n͎̣̫̞͜ͅ ̰̺͊̽̈́̍ͭ́͝museum̖̟̹̮͖͓̤̅͒ͭ͆. We have more than enough reason to trust that these are Rolatide devices.
 <br/><br/>
 J.L.: What would Rolatide want with these relics?
 <br/><br/>
-S.L.: That’s a question we hope to answer soon. Jaxson, you’ll be going with Lemmy to the library. When we get Ỵ̌̔͊̋ͫ͑ͣiͩͥ̌̍̊ͭͭ͜n̲̏m̵̹͚̲͍̠̱̥a̷͔͇̤̱̦̲ͭͣ͑͊̉̓o͟, we’ll be able to run some tests.
+S.L.: That’s a question we hope to answer soon. D'valin, you’ll be going with Torple to the library. When we get XuHai, we’ll be able to run some tests.
 <br/><br/>
-L.M.: Tests? On a wall scroll?
+L.M.: Tests? On a teapot?
 <br/><br/>
 S.L.: I think I know why Rolatide is interested in these animals, but I can’t know for certain until we have it here. Bolnaldo, did you get a chance to meet Franklin at the Lotus Blue?
 <br/><br/>
